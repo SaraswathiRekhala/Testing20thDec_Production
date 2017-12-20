b99fdf162ebe986a9ea5bd39b1d3f36398d7b391
saraswathi rekhala11 <saraswathi.rekhala+11@wavemaker.com>
2017-12-20 08:49:21 +0000

    V1 New files added - [OracleTest,HeartRateService,WMSTUDIO,fdaService,hrdb,data,fonts,lib,vermillion,vermillion] Files modified - [Main,securityService,WEB-INF,profiles,webapp,.wmproject.properties,pom.xml,pages-config.json] Removed files - [fonts,material]

---
+++ src/main/webapp/pages/OracleTest/OracleTest.js
@@ -0,0 +1,60 @@
+Application.$controller("OracleTestPageController", ["$scope", function ($scope) {
+    "use strict";
+
+    /* perform any action on widgets/variables within this block */
+    $scope.onPageReady = function () {
+        /*
+         * variables can be accessed through '$scope.Variables' property here
+         * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
+         * $scope.Variables.loggedInUser.getData()
+         *
+         * widgets can be accessed through '$scope.Widgets' property here
+         * e.g. to get value of text widget named 'username' use following script
+         * '$scope.Widgets.username.datavalue'
+         */
+    };
+
+}]);
+
+
+Application.$controller("ServerDefinedTableTable1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("ServerDefinedTableLiveForm1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("wmstudioexecutesvinsertqueryserverdefDialogController", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("AlltypesNewChangesFilter1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("Table1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("LiveForm1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
